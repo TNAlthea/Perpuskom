e443be8ca26430da831d6cce9f3d5a31abfff394
TNAlthea <sabiannaya@yahoo.com>
2022-12-30 18:14:15 +0700

    2nd

---
+++ lib/pages/navpages/genre_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class GenrePage extends StatelessWidget {
+  const GenrePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+          child: Text(
+              "Genre Page"
+          ),
+        )
+    );
+  }
+}
